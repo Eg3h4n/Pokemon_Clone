53913bd3229d903dbad7a7d3583272b4c62a84e9
Egehan Gur <53318302+Eg3h4n@users.noreply.github.com>
2024-09-11 14:00:45 +0300

    Initial commit

---
+++ Assets/Scripts/Pokemons/PokemonBase.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonBase : ScriptableObject
+{
+    [SerializeField] string name;
+
+    [TextArea]
+    [SerializeField] string description;
+
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite;
+
+    [SerializeField] PokemonType type1;
+    [SerializeField] PokemonType type2;
+
+}
+
+public enum PokemonType
+{
+    None,
+    Normal,
+    Fire,
+    Water,
+    Electric,
+    Grass,
+    Ice,
+    Fighting,
+    Poison,
+    Ground,
+    Flying,
+    Psychic,
+    Bug,
+    Rock,
+    Ghost,
+    Dragon,
+    Dark,
+    Steel,
+    Fairy
+}
